c0b62e56d9e6613a968cdb266969e03c3724da11
ajcwebdev <acampolo5@gmail.com>
2020-05-29 07:23:14 -0600

    Delete

--- pintereach/src/components/App.js
+++ pintereach/src/components/App.js
@@ -1,31 +1,36 @@
 import React, {useState, useEffect} from "react";
 import {Route, Link} from "react-router-dom";
-import Article from "./Articles/Article";
 import { axiosWithAuth } from '../utils/axiosWithAuth';
-import UpdateArticle from "./Articles/UpdateArticle";
-import AddArticle from "./Articles/AddArticle";
 import Login from "./Login";
 import Register from "./Register";
-import {ArticleContext} from '../context/ArticleContext'
 
 const App = () => {
-  const [articleList, setArticleList] = useState([ArticleContext]);
-  const [credentials, setCredential] = useState([ArticleContext]);
-  const [userId, setUserId] = useState({user_id: null})
+const [article, setArticle] = useState()
 
-  const getArticleList = () => {
-    axiosWithAuth()
-      .get("https://pintereach-1.herokuapp.com/api/articles")
-      .then(res => {
-        setArticleList(res.data.articles)})
-      .catch(err => console.log(err.res));
-  };
+axiosWithAuth()
+.get("https://pintereach-1.herokuapp.com/api/articles")
+.then(res => {
+  // console.log(res.data.articles)
+  // setArticle(res.data.articles)
+})
+.catch(err => console.log(err.res));
+  // const [articleList, setArticleList] = useState([ArticleContext]);
+  // const [credentials, setCredential] = useState([ArticleContext]);
+  // const [userId, setUserId] = useState({user_id: null})
 
-  useEffect(() => {getArticleList();}, []);
+  // const getArticleList = () => {
+  //   axiosWithAuth()
+  //     .get("https://pintereach-1.herokuapp.com/api/articles")
+  //     .then(res => {
+  //       setArticleList(res.data.articles)})
+  //     .catch(err => console.log(err.res));
+  // };
+
+  // useEffect(() => {getArticleList();}, []);
   return (
     <>
-    <ArticleContext.Provider value={{articleList, setArticleList, credentials, setCredential, userId, setUserId}}>
-      <Link to="/"><button>Home</button></Link>
+    {/* <ArticleContext.Provider value={{articleList, setArticleList, credentials, setCredential, userId, setUserId}}> */}
+      {/* <Link to="/"><button>Home</button></Link>
       <Link to="/add-article"><button>Add Article</button></Link>
       <Link to="/login"><button>Login</button></Link>
       <Link to="/register"><button>Register</button></Link>
@@ -46,10 +51,10 @@ const App = () => {
 
       <Route path="/add-article">
         <AddArticle articleList={articleList} setArticleList={setArticleList} userId={userId} setUserId={setUserId} />
-      </Route>
-    </ArticleContext.Provider>
+      </Route> */}
+    {/* </ArticleContext.Provider> */}
     </>
   );
-};
-
+;
+    }
 export default App;
